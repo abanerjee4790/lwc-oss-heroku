a3ef5c565b74a3b22973f92fc57bfcf4e73dfc50
abbanerjee <abhishek.d.banerjee@capgemini.com>
2020-05-09 21:47:01 +0530

    Debug Errors

--- src/client/modules/my/button/button.js
+++ src/client/modules/my/button/button.js
@@ -2,9 +2,9 @@ import LightningElementSLDS from 'util/lightningElementSLDS';
 
 export default class Button extends LightningElementSLDS {
     getMessage() {
-        fetch('https://fierce-crag-08924.herokuapp.com/api/v1/getMessage')
+        fetch('https://fierce-crag-08924.herokuapp.com/api/v1/endpoint')
             .then((response) => {
-                return response.json();
+                return response;
             })
             .then((result) => {
                 // eslint-disable-next-line no-alert
