3d5bb537ff57acbd501d81bfdc6a2684a5289156
abbanerjee <abhishek.d.banerjee@capgemini.com>
2020-05-10 22:33:58 +0530

    Debug Errors

--- scripts/server.js
+++ scripts/server.js
@@ -1,13 +1,26 @@
-const path = require('path');
+// Simple Express server setup to serve the build output
+const compression = require('compression');
+const helmet = require('helmet');
 const express = require('express');
-
-const customServer = require(path.resolve('./src/server/'));
+const path = require('path');
 
 const app = express();
+app.use(helmet());
+app.use(compression());
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
+const DIST_DIR = './dist';
+const customServer = require(path.resolve('./src/server/'));
+
+app.use(express.static(DIST_DIR));
 
 customServer(app);
 
-app.listen(3002, () => {
-    // eslint-disable-next-line no-console
-    console.log('Yay, local server started');
-});
+/*app.use('*', (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});*/
+
+app.listen(PORT, () =>
+    console.log(`✅  Server started: http://${HOST}:${PORT}`)
+);
