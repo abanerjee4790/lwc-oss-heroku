faede813a22563543f73e7fe1810860234c5881e
abbanerjee <abhishek.d.banerjee@capgemini.com>
2020-05-11 02:50:20 +0530

    Debug

--- scripts/server.js
+++ scripts/server.js
@@ -15,7 +15,8 @@ const DIST_DIR = './dist';
 app.use(express.static(DIST_DIR));
 
 app.use('*', (req, res) => {
-    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+    //res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+    res.send('Hello LWC');
 });
 
 app.get('/api/v1/getMessage', (req, res) => {
